0e10ed06670ba1b5db71972e1a7e7d57609add49
teide <tee@soprasteria.com>
2018-04-12 22:47:30 +0200

    GO-824: Rettet ny typo

--- src/main/resources/db/oracle/migration/V3__create_dvh_trigger_delete.sql
+++ src/main/resources/db/oracle/migration/V3__create_dvh_trigger_delete.sql
@@ -4,7 +4,7 @@ AFTER DELETE ON SAK
 FOR EACH ROW BEGIN
  INSERT INTO sak_gr.sak_gr
  (sak_gr_id, sak_id, tema, applikasjon, fagsaknr, opprettet_av, opprettet_tidspunkt, dato_overfort_grensesnitt, endring_type)
- VALUES (gsak_gr.SAK_SEQ.nextval, :old.id, :old.tema, :old.applikasjon, :old.fagsaknr, :old.opprettet_av, :old.opprettet_tidspunkt, CURRENT_TIMESTAMP, 'D');
+ VALUES (sak_gr.SAK_SEQ.nextval, :old.id, :old.tema, :old.applikasjon, :old.fagsaknr, :old.opprettet_av, :old.opprettet_tidspunkt, CURRENT_TIMESTAMP, 'D');
 
  END;
 
