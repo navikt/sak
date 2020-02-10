fc36a4ffaf640be5dca76161be330c0e2bb60503
erlend-axelsson <erlend.axelsson@gmail.com>
2020-02-10 16:50:56 +0100

    MMA-4107 ADD config files for naiserator migration

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,6 @@
-naisPipeline {
+naiseratorPipeline {
     applicationName = "sak"
     dockerfileDir = "."
+    deployEnvironmentT = "t0,t4,t5,t6,t8"
+    deployEnvironmentQ = "q0,q1,q2,q5,q8"
 }
---
+++ export-vault-secrets.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+if test -f /var/run/secrets/nais.io/sakDS/username;
+then
+    echo "Setting SAKDS_USERNAME"
+    export SAKDS_USERNAME=$(cat /var/run/secrets/nais.io/sakDS/username)
+fi
+
+if test -f /var/run/secrets/nais.io/sakDS/password;
+then
+    echo "Setting SAKDS_PASSWORD"
+    export SAKDS_***passord=gammelt_passord***)
+fi
+
+if test -f /var/run/secrets/nais.io/ldap/username;
+then
+    echo "Setting LDAP_USERNAME"
+    export LDAP_USERNAME=$(cat /var/run/secrets/nais.io/ldap/username)
+fi
+
+if test -f /var/run/secrets/nais.io/ldap/password;
+then
+    echo "Setting LDAP_PASSWORD"
+    export LDAP_***passord=gammelt_passord***)
+fi
+
+if test -f /var/run/secrets/nais.io/srvsak/username;
+then
+    echo "Setting SRVSAK_USERNAME"
+    export SRVSAK_USERNAME=$(cat /var/run/secrets/nais.io/srvsak/username)
+fi
+
+if test -f /var/run/secrets/nais.io/srvsak/password;
+then
+    echo "Setting SRVSAK_PASSWORD"
+    export SRVSAK_***passord=gammelt_passord***)
+fi
