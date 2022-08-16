94de324dd63cf958cbf0c54c2a75614e6010a5c8
S161464 <hakon.snotun@nav.no>
2022-08-16 12:37:38 +0200

    MMA-6277: PR-comments

--- app/src/main/java/no/nav/sak/app/Application.java
+++ app/src/main/java/no/nav/sak/app/Application.java
@@ -1,7 +1,6 @@
 package no.nav.sak.app;
 
 import no.nav.sak.SakApplication;
-import no.nav.sak.infrastruktur.AliveCheckServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,11 +10,7 @@ import org.springframework.context.annotation.Import;
 public class Application {
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(Application.class, args);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
+		SpringApplication.run(Application.class, args);
 	}
 
 }
