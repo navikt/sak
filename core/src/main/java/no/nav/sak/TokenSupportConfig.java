87629996542abf4d92d2d6ffb68df232c18caf1c
Alexander Petrov <alexander.petrov@nav.no>
2022-11-14 14:53:14 +0100

    legg til basis token support konfigurasjon

---
+++ core/src/main/java/no/nav/sak/TokenSupportConfig.java
@@ -0,0 +1,53 @@
+package no.nav.sak;
+
+
+import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration;
+import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever;
+import no.nav.security.token.support.core.context.TokenValidationContextHolder;
+import no.nav.security.token.support.filter.JwtTokenValidationFilter;
+import no.nav.security.token.support.jaxrs.JaxrsTokenValidationContextHolder;
+import no.nav.security.token.support.jaxrs.servlet.JaxrsJwtTokenValidationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
+
+@Configuration
+public class TokenSupportConfig {
+
+
+    @Bean
+    public MultiIssuerProperties multiIssuerProperties () {
+        return new MultiIssuerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtTokenValidationFilter> oidcTokenValidationFilterBean(
+            MultiIssuerConfiguration config) {
+        return new FilterRegistrationBean<>(new JaxrsJwtTokenValidationFilter(config));
+    }
+
+
+    @Bean
+    public MultiIssuerConfiguration multiIssuerConfiguration(MultiIssuerProperties issuerProperties,ProxyAwareResourceRetriever resourceRetriever) {
+        return new MultiIssuerConfiguration(issuerProperties.getIssuer(), resourceRetriever);
+    }
+
+
+    @Bean
+    public ProxyAwareResourceRetriever oidcResourceRetriever() {
+        return new ProxyAwareResourceRetriever();
+    }
+
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+    @Bean
+    public TokenValidationContextHolder jaxrsContextHolder() {
+        return JaxrsTokenValidationContextHolder.getHolder();
+    }
+
+}
